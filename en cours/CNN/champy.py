00ccce13edeaead4879e7e3c050de4e13d3c109e
radiuum88 <radiuum@hotmail.com>
2025-12-21 23:45:41 +0100

    creating a new file for my "library" neurones

--- en cours/CNN/champy.py
+++ en cours/CNN/champy.py
@@ -1,23 +1,4 @@
-class Neurone :
-
-    def __init__(self, coefficients, func) :
-
-        self.__coefs = coefficients
-        self.__func = func
-
-    def change_coefs(self, new_coefs) :
-
-        self.__coefs = new_coefs
-
-    def sortie(self, inputs) :
-
-        assert len(inputs) == len(self.__coefs)
-        
-        for index in len(range(inputs)) :
-
-            inputs[index] *= self.__coefs[index]
-
-        return self.__func(inputs)
+from neurones import *
 
 def produit(termes) :
     a,b = termes
---
+++ en cours/CNN/neurones.py
@@ -0,0 +1,20 @@
+class Neurone :
+
+    def __init__(self, coefficients, func) :
+
+        self.__coefs = coefficients
+        self.__func = func
+
+    def change_coefs(self, new_coefs) :
+
+        self.__coefs = new_coefs
+
+    def sortie(self, inputs) :
+
+        assert len(inputs) == len(self.__coefs)
+        
+        for index in len(range(inputs)) :
+
+            inputs[index] *= self.__coefs[index]
+
+        return self.__func(inputs)
