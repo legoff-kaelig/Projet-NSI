0d2dcf1da59dea97faead3ba7fd452b4f2dc71f3
radiuum88 <radiuum@hotmail.com>
2025-12-21 23:59:59 +0100

    Commit de sauvegarde
    Ajout de la fonction sigmoïde dans la librairie "neurones"  et commencement de la création d'un réseau de neurones de test

--- en cours/CNN/neurones.py
+++ en cours/CNN/neurones.py
@@ -1,6 +1,23 @@
+import math
+
+def sigmoid(x):
+
+    sig = 1 / (1 + math.exp(-x))
+    return sig
+
+def func_de_base_somme(termes) :
+
+        resultat = 0
+
+        for nombre in termes :
+
+            resultat += nombre
+
+        return resultat
+
 class Neurone :
 
-    def __init__(self, coefficients, func) :
+    def __init__(self, coefficients, func = func_de_base_somme) :
 
         self.__coefs = coefficients
         self.__func = func
@@ -17,4 +34,4 @@ class Neurone :
 
             inputs[index] *= self.__coefs[index]
 
-        return self.__func(inputs)
+        return sigmoid(self.__func(inputs))
