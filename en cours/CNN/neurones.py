59e52ed69f516722a9df13e6c09e1b1b6063e24c
radiuum88 <radiuum@hotmail.com>
2025-12-22 12:49:18 +0100

    Début de la commentation des fonctions de la librairie neurones

--- en cours/CNN/neurones.py
+++ en cours/CNN/neurones.py
@@ -2,23 +2,41 @@ from random import random
 import math
 
 def sigmoid(x):
+    """
+    Entrée : <x> un réel
+
+    Sortie : un flottant strictement copris entre 0 et 1
+    """
 
     sig = 1 / (1 + math.exp(-x))
     return sig
 
 def func_de_base_somme(termes) :
+    """
+    Entrée : <termes> une liste de réels
 
-        resultat = 0
+    Sortie : La somme de tous ces termes
+    """
+    resultat = 0
 
-        for nombre in termes :
+    for nombre in termes :
 
-            resultat += nombre
+        resultat += nombre
 
-        return resultat
+    return resultat
 
 class Neurone :
 
     def __init__(self, coefficients, func = func_de_base_somme, biais = 0) :
+        """
+        Entrée :
+
+            :coefficients: La liste des coefficients du neurone
+            :func: La fonction que va effectuer le neurone, de base une somme
+            :biais: Le biais du neurone
+
+        Action : Création des attributs éponymes aux entrées pour notre neurone
+        """
 
         self.__coefs = coefficients
         self.__func = func
