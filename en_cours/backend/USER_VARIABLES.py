b4a04757a387132bfdb9e0aa97a1e94be1ece98e
Kaelig <151878337+legoff-kaelig@users.noreply.github.com>
2026-01-22 12:21:51 +0100

    Refactor weather data handling and cleanup user variables
    Updated weather_data.py to load location data from a specified path, extract variables, and prepare for API requests using Tomorrow.io. Cleaned up USER_VARIABLES.py by removing embedded JSON and adding import for FUNCTIONS. Minor .gitignore adjustment and added temp.json for sample weather data.

--- en_cours/backend/USER_VARIABLES.py
+++ en_cours/backend/USER_VARIABLES.py
@@ -2,27 +2,11 @@
 #  User Variables Configuration  #
 ##################################
 
+import FUNCTIONS as func
+
 is_24_hour_format = True
 is_metric_system = True
 setup_wizard_done = True
 
-
-[
-  {
-    "ville_id": "13474",
-    "ville_nom": "LA CHAPELLE-CHAUSSEE",
-    "ville_nom_simple": "la chapelle chaussee",
-    "ville_nom_reel": "La Chapelle-Chaussée",
-    "ville_code_postal": "35630",
-    "ville_code_commune": "35058",
-    "ville_arrondissement": "3",
-    "ville_canton": "04",
-    "ville_population_2010": "1146",
-    "ville_population_1999": "763",
-    "ville_surface": "14.76",
-    "ville_longitude_deg": "-1.85",
-    "ville_latitude_deg": "48.2667",
-    "ville_zmin": "84",
-    "ville_zmax": "148"
-  }
-]
+# if is_24_hour_format:
+#     timestamp = func.to_24h(timestamp)
--- en_cours/backend/weather_data.py
+++ en_cours/backend/weather_data.py
@@ -1,15 +1,28 @@
 # https://capacitorjs.com/docs/apis/geolocation#interfaces
 
 import json
+import requests
+
 import USER_VARIABLES
 import FUNCTIONS as func
+from private.API_KEY import API_KEY
 
-with open('locations.json', 'r', encoding='utf-8') as file:
+locations_path = 'en_cours\\backend\\locations.json'
+
+# Get datas about the user from the JSON files in a DICT
+with open(locations_path, 'r', encoding='utf-8') as file:
     datas = json.load(file)
 
+# Extract datas.dict values into variables
 latitude = datas[0]["latitude"]
 longitude = datas[0]["longitude"]
 timestamp = datas[0]["timestamp"].split(", ")[1]
 
-if USER_VARIABLES.is_24_hour_format:
-    timestamp = func.to_24h(timestamp)
+# API request
+url = f'https://api.tomorrow.io/v4/weather/forecast?location={latitude},{longitude}&apikey={API_KEY}'
+fields = [
+    'humidity', 'freezingRainIntensity',
+    'precipitationProbability'
+]
+
+print(timestamp)
