2cac58d011f598a6106a6f9dd017a65e159ab55a
JxstGeek <152151158+JxstGeek@users.noreply.github.com>
2026-02-11 01:30:43 +0100

    .

--- sources/frontend/scan.js
+++ sources/frontend/scan.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("file-input");
   const previewContainer = document.getElementById("preview-container");
   const previewImage = document.getElementById("preview-image");
+  const validateBtn = document.getElementById("validate-btn");
 
   // Gérer la sélection de fichier
   input.addEventListener("change", () => {
     const file = input.files[0];
-    const nameImage = file.name;
     if (!file) return;
     
     // Vérifier que le fichier est une image
@@ -24,21 +24,5 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     reader.readAsDataURL(file);
-
-    const fs = require('fs');
-
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => console.log(data))
-
-    var donnees = JSON.parse(data);
-
-    donnees.nom = nameImage;
-    donnees.run = true;
-
-    const jsonString = JSON.stringify(donnees, null, 2);
-
-    fs.writeFileSync('donnees.json', jsonString, 'utf8');
-
   });
 });
